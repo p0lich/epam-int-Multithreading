16708660556a5841c67751fb86c19b18a0c54fc2
p0lich <a.polikarpov2014@gmail.com>
2021-12-29 15:15:06 +0400

    writed tasks

---
+++ EPAM.Multithreading/EPAM.Multithreading.ParallelMultiply/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EPAM.Multithreading.ParallelMultiply
+{
+    internal class Program
+    {
+        private static Random _random = new Random();
+
+        static void Main(string[] args)
+        {
+            MatrixInitialize(out int[,] matrix1, 5, 5);
+            MatrixInitialize(out int[,] matrix2, 5, 5);
+
+            PrintMatrix(matrix1);
+            Console.WriteLine("---------");
+
+            PrintMatrix(matrix2);
+            Console.WriteLine("---------");
+
+            int[,] multiplyResult = ParallelMatrixMultiply(matrix1, matrix2);
+
+            PrintMatrix(multiplyResult);
+            Console.WriteLine("---------");
+        }
+
+        private static void MatrixInitialize(out int[,] matrix, int matrixWidth, int matrixHeight)
+        {
+            matrix = new int[matrixWidth, matrixHeight];
+
+            for (int i = 0; i < matrixWidth; i++)
+            {
+                for (int j = 0; j < matrixHeight; j++)
+                {
+                    matrix[i, j] = _random.Next(0, 10);
+                }
+            }
+        }
+
+        private static int[,] ParallelMatrixMultiply(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+            Parallel.For(0, matrix1.GetLength(0), i =>
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    result[i,j] = matrix1[i,j] * matrix2[i,j];
+                }
+            });
+
+            return result;
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ EPAM.Multithreading/EPAM.Multithreading.TasksChain/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPAM.Multithreading.TasksChain
+{
+    internal class Program
+    {
+        private static Random _random = new Random();
+
+        static async Task Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            Task<int[]> createArray = ArrayInitialize(array);
+            Task<int[]> multiplyArray = MulitplyArray(array, 5);
+            Task<int[]> sortArray = SortArray(array);
+            Task<int> getAverage = GetAverage(array);
+
+            Task<int[]>[] tasks = new Task<int[]>[] { createArray, multiplyArray, sortArray};
+
+            Task[] processingTasks = tasks.Select(async t =>
+            {
+                var result = await t;
+                TaskPrint(t);
+            }).ToArray();
+
+            await Task.WhenAll(processingTasks);
+        }
+
+        private static async Task<int[]> ArrayInitialize(int[] array)
+        {
+            await Task.Factory.StartNew(() => {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = _random.Next(0, 100);
+                }
+            });
+
+            return array;
+        }
+
+        private static async Task<int[]> MulitplyArray(int[] array, int value)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] *= value;
+                }
+            });
+
+            return array;
+        }
+
+        private static async Task<int[]> SortArray(int[] array)
+        {
+            await Task.Factory.StartNew(() => { Array.Sort(array); });
+            
+            return array;
+        }
+
+        private static async Task<int> GetAverage(int[] array)
+        {
+            int average = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                average += array[i];
+            }
+
+            return average / array.Length;
+        }
+
+        private static void TaskPrint(Task<int[]> array)
+        {
+            Console.WriteLine("---------------------------------------");
+
+            for (int i = 0; i < array.Result.Length; i++)
+            {
+                Console.Write(array.Result[i] + " ");
+            }
+
+            Console.WriteLine("\n---------------------------------------");
+        }
+    }
+}
