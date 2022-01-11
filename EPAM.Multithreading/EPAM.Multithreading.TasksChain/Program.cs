95456f90297d42d48294283fd15424061a1d865e
p0lich <a.polikarpov2014@gmail.com>
2022-01-11 18:18:58 +0400

    fix issue with chain tasks

--- EPAM.Multithreading/EPAM.Multithreading.TasksChain/Program.cs
+++ EPAM.Multithreading/EPAM.Multithreading.TasksChain/Program.cs
@@ -13,20 +13,71 @@ namespace EPAM.Multithreading.TasksChain
         {
             int[] array = new int[10];
 
-            Task<int[]> createArray = ArrayInitialize(array);
-            Task<int[]> multiplyArray = MulitplyArray(array, 5);
-            Task<int[]> sortArray = SortArray(array);
-            Task<int> getAverage = GetAverage(array);
-
-            Task<int[]>[] tasks = new Task<int[]>[] { createArray, multiplyArray, sortArray};
-
-            Task[] processingTasks = tasks.Select(async t =>
+            var chainTask = Task.Run(() => 
             {
-                var result = await t;
-                TaskPrint(t);
-            }).ToArray();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = _random.Next(0, 100);
+                    Console.Write(array[i] + " ");
+                }
 
-            await Task.WhenAll(processingTasks);
+                Console.WriteLine();
+            });
+
+            await chainTask.ContinueWith(_ => 
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] *= _random.Next(1, 11);
+                    Console.Write(array[i] + " ");
+                }
+
+                Console.WriteLine();
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            await chainTask.ContinueWith(_ =>
+            {
+                Array.Sort(array);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                
+                Console.WriteLine();
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            await chainTask.ContinueWith(_ =>
+            {
+                int average = 0;
+                for (int i = 0;i < array.Length; i++)
+                {
+                    average += array[i];
+                }
+
+                average /= array.Length;
+                Console.WriteLine(average);
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            //Task<int[]> createArray = ArrayInitialize(array);
+            //Task<int[]> multiplyArray = MulitplyArray(array, 5);
+            //Task<int[]> sortArray = SortArray(array);
+
+            //Task<int> getAverage = GetAverage(array);
+
+            //Task<int[]>[] tasks = new Task<int[]>[] { createArray, multiplyArray, sortArray };
+
+            //Task[] processingTasks = tasks.Select(async t =>
+            //{
+            //    var result = await t;
+            //    ArrayPrint(t.Result);
+            //}).ToArray();
+
+            //var result = await Task.WhenAll(tasks);
+
+            //for (int i = 0; i < result.Length; i++)
+            //{
+            //    ArrayPrint(result[i]);
+            //}
         }
 
         private static async Task<int[]> ArrayInitialize(int[] array)
@@ -73,13 +124,13 @@ namespace EPAM.Multithreading.TasksChain
             return average / array.Length;
         }
 
-        private static void TaskPrint(Task<int[]> array)
+        private static void ArrayPrint(int[] array)
         {
             Console.WriteLine("---------------------------------------");
 
-            for (int i = 0; i < array.Result.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(array.Result[i] + " ");
+                Console.Write(array[i] + " ");
             }
 
             Console.WriteLine("\n---------------------------------------");
